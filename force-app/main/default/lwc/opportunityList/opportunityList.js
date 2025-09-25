140a4b7850e1027e7c7c74fa2b2561c012b62986
Every Grefaldeo <everly.grefaldeo@gmail.com>
2025-09-25 15:24:38 +0800

    Fix row action bug and add modal for editing StageName

--- force-app/main/default/lwc/opportunityList/opportunityList.js
+++ force-app/main/default/lwc/opportunityList/opportunityList.js
@@ -1,10 +1,17 @@
-import { LightningElement,wire } from 'lwc';
+import { LightningElement,wire,track } from 'lwc';
 import {ShowToastEvent} from 'lightning/platformShowToastEvent'
 import {NavigationMixin} from 'lightning/navigation';
 import getOpportunities from '@salesforce/apex/OpportunityListController.getOpportunities';
+import {updateRecord} from 'lightning/uiRecordApi';
+import {refreshApex} from '@salesforce/apex'
+import StageName from '@salesforce/schema/Opportunity.StageName';
 
 export default class OpportunityList extends NavigationMixin(LightningElement) {
     @wire(getOpportunities) opportunityList;
+        isModalOpen =false;
+        selectedRecord;
+        stageValue;
+
 
     columns= [
         {label:'Name',fieldName: 'Name', type:'text'},
@@ -18,19 +25,20 @@ export default class OpportunityList extends NavigationMixin(LightningElement) {
 
     getRowActions(row,doneCallBack){
         const actions = [
-            {label:'View',name:'view'}
+            {label:'View',name:'view'},
+            {label:'Edit Stage',name:'editStage'}
 
         ];
         doneCallBack(actions);
     }
-    handleView(event){
+   /* handleView(event){
         event.preventDefault();   // 👈 stop the default button behavior
         event.stopPropagation();  // 👈 stop event bubbling just in case
         const oppId= event.target.dataset.id;
         const opp=this.opportunityList.find(o => o.Id ===oppId);
         console.log(opp.Id + ' ' + opp.Name);
-    }
-    viewItem(event){
+    }*/
+    handleRowAction(event){
         const record= event.detail.row;
         const actionName = event.detail.action.name;
         if(actionName==='view'){
@@ -40,18 +48,75 @@ export default class OpportunityList extends NavigationMixin(LightningElement) {
                 message: `You clicked on ${record.Name} (${record.StageName})`,
                 variant:'Info'
                 })
-            )
-        }
-        this[NavigationMixin.Navigate]({
+            );
+            this[NavigationMixin.Navigate]({
             type: 'standard__recordPage',
             attributes:{
                 recordId: record.Id,
                 objectApiName:'Opportunity',
                 actionName: 'view'
             }
-        })
+        });
+
+        }
+        if(actionName==='editStage'){
+            this.selectedRecord =record;
+            this.stageValue=record.StageName;
+            this.isModalOpen = true;
+
+        }
+
 
 
     }
+    handleStageChange(event){
+        this.stageValue=event.target.value;
+
+    }
+    closeModal(){
+        this.isModalOpen=false;
+    }
+    get stageOptions(){
+        return [
+            {label: 'Prospecting', value: 'Prospecting'},
+            {label: 'Qualification', value: 'Qualification'},
+            {label: 'Needs Analysis', value: 'Needs Analysis'},
+            {label: 'Value Proposition', value: 'Value Proposition'},
+            {label: 'Closed Won', value: 'Closed Won'},
+            {label: 'Closed Lost', value: 'Closed Lost'},
+        ]
+    }
+    handleSave(){
+        const fields = {
+            Id: this.selectedRecord.Id,
+            StageName: this.stageValue
+        };
+        updateRecord({fields})
+            .then(
+                ()=> {
+                    this.dispatchEvent(
+                        new ShowToastEvent({
+                            title: 'Success',
+                            message:'Opportunity Stage updated',
+                            variant:'success'
+                        })
+                    );
+                    this.isModalOpen=false;
+                    return refreshApex(this.opportunityList);
+                }
+            )
+            .catch(error =>{
+                this.dispatchEvent(
+                    new ShowToastEvent({
+                        title: 'Error updating record',
+                        message: error.body.message,
+                        variant: 'error'
+                    })
+                );
+            }
+
+            );
+    }
+
 
 }
